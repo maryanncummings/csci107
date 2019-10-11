290e4743ad1ecabb965be2d7c52cf790fe1eb55f
maryanncummings <mary.cummings1@montana.edu>
2019-10-11 12:21:59 -0600

    Create func3example.py

---
+++ week_of_10-7/func3example.py
@@ -0,0 +1,9 @@
+#func3example.py
+import math
+
+num = int(input("Enter number: "))
+power = int(input("Enter power to raise number to: "))
+
+result = pow(num,power)
+print("The result is " + str(result))
+          
