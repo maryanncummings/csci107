7bbee4ed89498cd5e83d21233e74dd9d821241c3
maryanncummings <mary.cummings1@montana.edu>
2019-10-11 12:16:40 -0600

    changed drawpolygon1

--- week_of_10-7/drawpolygon1.py
+++ week_of_10-7/drawpolygon1.py
@@ -7,23 +7,37 @@ def drawPolygon(turt, sides, size):
         turt.forward(size)
         turt.left(angle)
 
-sides=int(input("Enter number of sides: "))
-size=50
-ts=turtle.Screen()
-ts.bgcolor("green")
+def main():
+    # read in number of sides
+    sides=int(input("Enter number of sides: "))
+    # hardwire size of each side
+    size=50
 
-myturt=turtle.Turtle()
-myturt.pensize(3)
-x_pos = -100.0
-y_pos = -100.0
-for i in range(3):
-    myturt.penup()
-    myturt.setpos(x_pos, y_pos)
-    myturt.pendown()
-    drawPolygon(myturt,sides,size)
-    x_pos = x_pos + 75
-    y_pos = y_pos + 75
-myturt.shape("blank")
+    #set up screen
+    ts=turtle.Screen()
+    ts.bgcolor("green")
+
+    #set up turtle
+    myturt=turtle.Turtle()
+    myturt.pensize(3)
+    x_pos = -100.0
+    y_pos = -100.0
+    
+    for i in range(3):
+        # move turtle to starting position
+        myturt.penup()
+        myturt.setpos(x_pos, y_pos)
+        myturt.pendown()
+        
+        drawPolygon(myturt,sides,size)
+
+        #change position variables
+        x_pos = x_pos + 75
+        y_pos = y_pos + 75
+        
+    myturt.shape("blank")
+
+main()
           
     
     
